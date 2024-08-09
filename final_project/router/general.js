06c7c6f40ec2cbfe7c048944a16b56e9d57dcfb5
limminghong <minghonglim@gmail.com>
2024-08-09 03:17:23 -0400

    get code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,26 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let booksByAuthor = {};
+
+  books.array.forEach(element => {
+    if (element.author == req.params.author) {
+        booksByAuthor.push(element);
+    }
+  });
+
+  return booksByAuthor;
 });
 
 // Get all books based on title
